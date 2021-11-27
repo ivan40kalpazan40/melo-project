5e7de7295f9dd4ae3171367dc9a42306dda2c959
Ivan Skordev <maymunskoa@yahoo.com>
2021-11-27 15:55:35 +0200

    melo app initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
+  const image =
+    'https://media.istockphoto.com/vectors/cartoon-rock-band-vector-vector-id621592618';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <Navbar />
+      <div className='ui container '>
+        <h1 class='ui center aligned header'>
+          melo
+          <div class='sub header'>
+            The social network to keep track of your favorite artists and music
+          </div>
+        </h1>
+        <div class='ui segment'>
+          <div class='ui left dividing rail'>
+            <div class='ui right aligned segment'>
+              <i class='music icon'> </i> Find your artist
+            </div>
+          </div>
+
+          <div class='ui three column center aligned  stackable padded grid'>
+            <div class='ui link cards'>
+              <div class='card column'>
+                <div class='image'>
+                  <img src={image} />
+                </div>
+                <div class='content'>
+                  <div class='header'>Matt Giampietro</div>
+                  <div class='meta'>
+                    <a>Friends</a>
+                  </div>
+                  <div class='description'>
+                    Matthew is an interior designer living in New York.
+                  </div>
+                </div>
+                <div class='extra content'>
+                  <span class='right floated'>Joined in 2013</span>
+                  <span>
+                    <i class='user icon'></i>
+                    75 Friends
+                  </span>
+                </div>
+              </div>
+              <div class='card column'>
+                <div class='image'>
+                  <img src={image} />
+                </div>
+                <div class='content'>
+                  <div class='header'>Molly</div>
+                  <div class='meta'>
+                    <span class='date'>Coworker</span>
+                  </div>
+                  <div class='description'>
+                    Molly is a personal assistant living in Paris.
+                  </div>
+                </div>
+                <div class='extra content'>
+                  <span class='right floated'>Joined in 2011</span>
+                  <span>
+                    <i class='user icon'></i>
+                    35 Friends
+                  </span>
+                </div>
+              </div>
+              <div class='card column'>
+                <div class='image'>
+                  <img src={image} />
+                </div>
+                <div class='content'>
+                  <div class='header'>Molly</div>
+                  <div class='meta'>
+                    <span class='date'>Coworker</span>
+                  </div>
+                  <div class='description'>
+                    Molly is a personal assistant living in Paris.
+                  </div>
+                </div>
+                <div class='extra content'>
+                  <span class='right floated'>Joined in 2011</span>
+                  <span>
+                    <i class='user icon'></i>
+                    35 Friends
+                  </span>
+                </div>
+              </div>
+              <div class='card column'>
+                <div class='image'>
+                  <img src={image} />
+                </div>
+                <div class='content'>
+                  <div class='header'>Elyse</div>
+                  <div class='meta'>
+                    <a>Coworker</a>
+                  </div>
+                  <div class='description'>
+                    Elyse is a copywriter working in New York.
+                  </div>
+                </div>
+                <div class='extra content'>
+                  <span class='right floated'>Joined in 2014</span>
+                  <span>
+                    <i class='user icon'></i>
+                    151 Friends
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div></div>
+
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <div class='ui inverted menu'>
+      <div class='header item'>: melo :::</div>
+      <a class='item'>Home</a>
+      <a class='item'>Jobs</a>
+      <a class='item'>Locations</a>
+
+      <div class='right menu'>
+        <a class='item'>Login</a>
+        <a class='item'>Register</a>
+        <a class='item'>Logout</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
