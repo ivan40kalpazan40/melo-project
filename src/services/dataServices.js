2b24933c1bd4309230eb513e3e561944ab78757f
Ivan Skordev <maymunskoa@yahoo.com>
2021-12-02 01:00:05 +0200

    fix timestamp field for documents

--- src/services/dataServices.js
+++ src/services/dataServices.js
@@ -1,9 +1,14 @@
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, Timestamp } from 'firebase/firestore';
 import { db } from '../config/firebase.config';
 
 export const addArtist = async (id, title, thumb, cover_image, type) => {
   const docRef = await addDoc(collection(db, 'artists'), {
-    id, title, thumb, cover_image, type
+    id,
+    title,
+    thumb,
+    cover_image,
+    type,
+    addedOn: Timestamp.now().toDate(),
   });
 
   console.log('Document written with ID: ', docRef.id);
